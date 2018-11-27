9451761aa95566962f004dc29afd062804a487fd
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-11-26 23:11:47 -0500

    spinner added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,25 +16,30 @@ export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
   public error: string;
+  public loading: boolean;
 
   constructor(private Auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.loading = false;
   }
 
   public submit() {
+    this.loading = true;
     this.Auth.getUserDetails(this.username, this.password)
       .subscribe(
         data => {
           const response = <response>data;
           this.error = '';
           console.log("Result: ", response);
+          this.loading = false;
           this.router.navigate(['data']);
           this.Auth.setLoggedIn(true);
         },
         err => {
           console.log("error: ", err);
           this.error = err.error;
+          this.loading= false;
           this.Auth.setLoggedIn(false);
         }
       );
